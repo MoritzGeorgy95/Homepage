47fb5aba56d245c51cdc9de083c0b5ddd627f5a5
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-03-09 17:01:38 +0100

    added slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular-Homepage';
+  images= ['img1.jpg', 'img2.jpg', 'img3.jpg'];
+  currentImage= 0;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+     setInterval(
+      () => {
+        this.currentImage++;
+        this.currentImage = this.currentImage % this.images.length
+      }, 4000
+     )
+  }
 }

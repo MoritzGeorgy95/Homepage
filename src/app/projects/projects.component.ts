b90343bee76f21e1212ffeac17a4b83c5058a23c
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-03-30 18:33:40 +0200

    added fade in animation

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+
+export class ProjectsComponent implements AfterViewInit  {
   openNewTab(url:string) {
     window.open(url, '_blank');
   }
+
+  constructor(private el: ElementRef) {}
+
+
+  @ViewChild('proj1') proj1: ElementRef;
+  @ViewChild('proj2') proj2: ElementRef;
+  @ViewChild('proj3') proj3: ElementRef;
+  @ViewChild('proj4') proj4: ElementRef;
+
+  ngAfterViewInit(){
+    const threshold = .3; // how much % of the element is in view
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {                 
+                  entry.target.classList.add('fly-in');
+                }
+                // else{
+                  
+                //   entry.target.classList.remove('fly-in');
+                // }
+            });
+        },
+        { threshold }
+    );
+    observer.observe(this.proj1.nativeElement);
+    observer.observe(this.proj2.nativeElement);
+    observer.observe(this.proj3.nativeElement);
+    observer.observe(this.proj4.nativeElement);
+  }
 }
+

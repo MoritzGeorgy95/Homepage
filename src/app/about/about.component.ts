fee8dfeafe401c236dfecbcaf38cc47db4beb85a
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-03-30 18:47:49 +0200

    finished fade in animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
-  
+export class AboutComponent implements AfterViewInit{
+
+
+  constructor(private el: ElementRef) {}
+
+
+  @ViewChild('el1') el1: ElementRef;
+  @ViewChild('el2') el2: ElementRef;
+
+
+
+  ngAfterViewInit() {
+    const threshold = .3; // how much % of the element is in view
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {                 
+                  entry.target.classList.add('fly-in');
+                }
+                // else{
+                  
+                //   entry.target.classList.remove('fly-in');
+                // }
+            });
+        },
+        { threshold }
+    );
+    observer.observe(this.el1.nativeElement);
+    observer.observe(this.el2.nativeElement);
+  }
 }

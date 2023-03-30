38d3650d8855740820f9b400a4e072e7c2120b74
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-03-30 13:18:52 +0200

    fixed mixed content issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Angular-Homepage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-Homepage');
+    expect(app.title).toEqual('Moritz Portfolio');
   });
 
   it('should render title', () => {

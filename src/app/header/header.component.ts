9ced7a0367f3c03a84aa73abc6521e4125876681
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-03-30 17:05:58 +0200

    added mobile navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,37 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  @Input() lightMode= false;
+  @Input() lightMode = false;
+
+  mobile = false;
+  menuOpen = false;
+
+  constructor() {
+    this.checkMobile();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkMobile();
+  }
+
+  private checkMobile() {
+    if (window.innerWidth < 700) {
+      this.mobile = true;
+    } else {
+      this.mobile = false;
+    }
+  }
+
+  openMobileMenu() {
+    this.menuOpen = true;
+  }
+
+  closeMobileMenu() {
+    this.menuOpen = false;
+  }
+
+  showPlaceholder() {
+    alert('Lebenslauf muss ich noch aktualisieren. In Arbeit');
+  }
 }
